5cb200dd5b9be3ccd1310f800a56eac7258ed46b
Adrian Frielinghaus <adrian.frielinghaus@gmail.com>
2020-11-03 14:32:17 +0200

    no message

--- src/Blauhaus.ClientActors/_Ioc/ServiceCollectionExtensions.cs
+++ src/Blauhaus.ClientActors/_Ioc/ServiceCollectionExtensions.cs
@@ -6,10 +6,28 @@ namespace Blauhaus.ClientActors._Ioc
 {
     public static class ServiceCollectionExtensions
     {
-        public static IServiceCollection AddActor<TActor>(this IServiceCollection services) where TActor : class, IActor
+        public static IServiceCollection AddActor<TActor>(this IServiceCollection services)
+            where TActor : class, IActor  
         {
+
             services.TryAddSingleton<IActorContainer<TActor>, ActorContainer<TActor>>();
-            services.AddTransient<TActor>();
+            
+            //must be transient because container manages lifecycle
+            services.AddTransient<TActor>();  
+
+            return services;
+        }
+
+
+        public static IServiceCollection AddActor<TActor, TActorImplementation>(this IServiceCollection services)
+            where TActor : class, IActor 
+            where TActorImplementation : class, TActor
+        {
+
+            services.TryAddSingleton<IActorContainer<TActor>, ActorContainer<TActor>>();
+            
+            //must be transient because container manages lifecycle
+            services.AddTransient<TActor, TActorImplementation>();  
 
             return services;
         }
