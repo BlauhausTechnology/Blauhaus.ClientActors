590c29767d3ef76489680b8ba0fa2e850f2fe55f
Adrian Frielinghaus <adrian.frielinghaus@gmail.com>
2020-11-30 09:04:55 +0200

    no message

--- src/Blauhaus.ClientActors.Abstractions/IModelActor.cs
+++ src/Blauhaus.ClientActors.Abstractions/IModelActor.cs
@@ -4,11 +4,10 @@ using Blauhaus.Common.Utils.Contracts;
 
 namespace Blauhaus.ClientActors.Abstractions
 {
-    public interface IModelActor<in TId, TModel> : IActor<TId> 
+    public interface IModelActor<in TId, TModel> : IActor<TId> , IPublish<TModel>
         where TModel : IId<TId>
     {
 
         Task<TModel> GetModelAsync();
-        Task<IDisposable> SubscribeAsync(Func<TModel, Task> handler);
     }
 }
