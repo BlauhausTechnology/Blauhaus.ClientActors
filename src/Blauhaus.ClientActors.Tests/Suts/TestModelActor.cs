69f7fe8c2251e8bba1cc074cd042c20a584ecf4e
Adrian Frielinghaus <adrian.frielinghaus@gmail.com>
2021-03-27 08:50:28 +0200

    no message

--- src/Blauhaus.ClientActors.Tests/Suts/TestModelActor.cs
+++ src/Blauhaus.ClientActors.Tests/Suts/TestModelActor.cs
@@ -10,5 +10,10 @@ namespace Blauhaus.ClientActors.Tests.Suts
         {
             return Task.FromResult<ITestModel>(new TestModel(Id));
         }
+
+        public async Task UpdateSubscribersWithCurrentModelAsync()
+        {
+            await UpdateModelAsync(x => x);
+        }
     }
 }
