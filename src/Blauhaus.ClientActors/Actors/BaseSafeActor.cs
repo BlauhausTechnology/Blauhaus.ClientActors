cf1b7be036253b584f9611a8be2aacd975d0b83a
Adrian Frielinghaus <adrian.frielinghaus@gmail.com>
2021-11-22 17:33:37 +0200

    no message

--- src/Blauhaus.ClientActors/Actors/BaseSafeActor.cs
+++ src/Blauhaus.ClientActors/Actors/BaseSafeActor.cs
@@ -8,16 +8,16 @@ namespace Blauhaus.ClientActors.Actors
 {
     public abstract class BaseActor : BasePublisher, IAsyncDisposable
     {
-        protected Task InvokeAsync(Action action) 
+        protected Task InvokeAsync(Action action, CancellationToken cancellationToken = default) 
             => Task.Run(action.Invoke);
 
-        protected Task<T> InvokeAsync<T>(Func<T> function) 
+        protected Task<T> InvokeAsync<T>(Func<T> function, CancellationToken cancellationToken = default) 
             => Task.Run(function.Invoke);
 
-        protected Task InvokeAsync(Func<Task> asyncAction) 
+        protected Task InvokeAsync(Func<Task> asyncAction, CancellationToken cancellationToken = default) 
             => asyncAction.Invoke();
 
-        protected Task<T> InvokeAsync<T>(Func<Task<T>> asyncFunction)
+        protected Task<T> InvokeAsync<T>(Func<Task<T>> asyncFunction, CancellationToken cancellationToken = default)
             => asyncFunction.Invoke();
 
 
